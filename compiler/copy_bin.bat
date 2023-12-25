431f31c453f7d8603212ab6e73d71e20cbd6c2c1
Peter Quiring <pquiring@gmail.com>
2023-12-25 09:44:12 -0500

    fix copy_bin.bat

--- compiler/copy_bin.bat
+++ compiler/copy_bin.bat
@@ -1,5 +1,5 @@
 @echo off
 
-copy src\bin\x64\release\net8.0\win-x64\*.exe ..\..\bin
-copy src\bin\x64\release\net8.0\win-x64\*.dll ..\..\bin
-copy src\bin\x64\release\net8.0\win-x64\*.json ..\..\bin
+copy src\bin\x64\release\net8.0\win-x64\*.exe ..\bin
+copy src\bin\x64\release\net8.0\win-x64\*.dll ..\bin
+copy src\bin\x64\release\net8.0\win-x64\*.json ..\bin
